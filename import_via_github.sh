2ce3e6b112cf1ba6b3931f38c105ee5d57776461
bonzzzy <114540588+bonzzzy@users.noreply.github.com>
2022-10-11 18:06:45 +0200

    Initial commit

---
+++ import_via_github.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# CONSEIL = Il est possible de renommer, pour plus de facilité d'utilisation, ce
+# fichier en « @i » par exemple :
+#
+#		mv import_via_github.sh @i
+#
+# Before using this file as a shell script you must change its access permissions :
+#
+# 		chmod u+rwx import_via_github.sh
+#
+# .. ou ( si le fichier a été renommé ) :
+#
+# 		chmod u+rwx @i
+#
+
+python import_via_github.py "skeleton.py" "my Check-Lists.py" "import_via_github.sh"
