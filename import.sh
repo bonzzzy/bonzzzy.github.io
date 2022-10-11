99590d1c07ec02bd8d2b9ef0b6606a284fc5d9c5
bonzzzy <114540588+bonzzzy@users.noreply.github.com>
2022-10-11 17:39:07 +0200

    Initial commit

---
+++ import.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Before using this file as a shell script you must change its access permissions
+#
+# 		chmod u+rwx import.sh
+#
+
+python import_via_github.py "skeleton.py" "my Check-Lists.py" "" .
